fae1c61e3bc4da9d043700c0f0cfff7ee694821b
zhangrunfeng <1602674061@qq.com>
2026-06-23 16:17:29 +0800

    Polish site icons and favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,16 @@
 import "../src/styles.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg?v=3" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg?v=3" type="image/svg+xml" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#f7f8fb" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
